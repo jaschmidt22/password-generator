4d95b09a0dd97132f2325424abf87e94ddf68563
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2023-11-05 21:05:24 -0700

    changed the order of functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,65 +7,72 @@ var specialList = "!@#$%^&*()_";
 var numberList = "0123456789";
 var characterPool = "";
 
+var length, uppercase, lowercase, special, number;
+
 // Add event listener to generate button
  generateBtn.addEventListener("click", writePassword);
 
+function getRandomItem(characterPool) {
+  return characterPool[Math.floor(Math.random() * characterPool.length)];
+}
  // Write password to the #password input
 function writePassword() {
  
-  //logic for prompting length, special, numeric, lower, and upper
-  //if lenth > 128 or length < 8 
-  //let user know they need to be within our constraints
-  //store resonse
-  var length = getPasswordLength() 
+  //logic for prompting length, upper, lower, special and numeric
+  
+  length = getPasswordLength() 
+
+  uppercase = confirm("Do you want upper case letters?");
+  if (uppercase) {
+  
+  var val = getRandomItem(uppercaseList);
+  console.log(val);
+  }
+  lowercase = confirm("Do you want lower case letters?");
+  if (lowercase) {
+  
+  var val = getRandomItem(lowercaseList);
+  console.log(val)
+  }
+  special = confirm("Do you want special characters?");
+  if (special) {
+  
+  var val = getRandomItem(specialList);
+  console.log(val)
+  }
+  number  = confirm("Do you want numbers?");
+  if (number) {
+  
+  var val = getRandomItem(numberList)
+  console.log(val)
+  }
+
+//generate the password
+var password = generatePassword(length, uppercase, lowercase, special, number);
+ 
+//display password in the #password field
+ var passwordText = document.querySelector("#password");
+ passwordText.value = password;
 }
-  function getPasswordLength() {
+
+function getPasswordLength() {
   while(true) {
    length = prompt("How many characters would you like?");
    console.log(length);
- //end the loop if criteria is satisfied
+ //if lenth > 128 or length < 8 
+//let user know they need to be within our constraints
+//store resonse
+//loop continues if criteria is not met
    if (length >= 8 && length <= 128 && !isNaN(length)) {
-      break; 
+      return parseInt(length); 
    } else {
      alert("Your password must be between 8 and 128 characters in length.");
    }
   }
-
- var uppercase = confirm("Do you want upper case letters?");
- if (uppercase) {
- 
- var val = getRandomItem(uppercaseList);
- console.log(val);
- }
- var lowercase = confirm("Do you want lower case letters?");
- if (lowercase) {
- 
- var val = getRandomItem(lowercaseList);
- console.log(val)
- }
- var special = confirm("Do you want special characters?");
- if (special) {
- 
- var val = getRandomItem(specialList);
- console.log(val)
- }
- var numbers  = confirm("Do you want numbers?");
- if (numbers) {
- 
- var val = getRandomItem(numberList)
- console.log(val)
- }
-  //write algo for creating password with the selected characters, constrained by length user selected   
-  
-  var password = generatePassword(length, uppercase, lowercase, special, numbers);
-  
-  //display password in the #password field
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
 }
 
-function generatePassword(length, uppercase, lowercase, special, numbers) {
- var characterPool = "";
+  function generatePassword(length, uppercase, lowercase, special, number) {
+    characterPool = "";
   
  if (uppercase) {
     characterPool += uppercaseList;
@@ -82,14 +86,16 @@ function generatePassword(length, uppercase, lowercase, special, numbers) {
   characterPool += specialList;
  }
 
- if (numbers) {
+ if (number) {
   characterPool += numberList
  }
-}
+
+ var password = "";
  for (index = 0; index < length; index++) { 
+  password += getRandomItem(characterPool);
 }
-function getRandomItem(characterPool) {
-  return characterPool[Math.floor(Math.random() * characterPool.length)];
+
+return password;
 
 }
-//return password;
+  
